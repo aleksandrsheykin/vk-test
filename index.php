1c76b569b55dab79a25e1363935e7ef85ac507b7
aleksandrsheykin <Lunatik_@bk.ru>
2016-07-25 15:22:47 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,51 @@
 <!DOCTYPE html>
-<!-- ГЏГ®Г¤ГЄasdfasdfasdfjqueryasdf -->
 <html>
 <head>
+    <script src='http://code.jquery.com/jquery-1.7.1.js'></script>
+    <script src="http://vk.com/js/api/xd_connection.js?2" type="text/javascript"></script>
+    <script src="js.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script>
+        // Функция получения друзей и вывода их на экран
+        function doIt(){
+                /*
+                VK.api('метод', {параметр: "значение"}, callback функция{
+                // работаем с полученными данными
+                });
 
+            VK.api("friends.get", {fields: "photo_medium"}, function(data) { 
+                // Действия с полученными данными 
+                for(c=0;c<data.response.length;c++){
+                    $("body").append("<img src='"+data.response[c].photo_medium+"'>");
+                }
+            });                
+                
+                */
+                
+            VK.init(function() { 
+                //alert('good');
+                VK.api("wall.post", {"message": "Hello!"}, function (data) {
+                    alert("Post ID:" + data.response.post_id);
+                });
+            }, function() { 
+                alert('failed');
+            }, '5.53');
+
+        }
+        $(document).ready(function(){
+            doIt();
+            //SetQuestion();
+            
+        });
+    </script>
 </head>
-<body style='text-align:center;padding-top:50px;' id='body'>
-<p>TEST</p>
-<iframe width="420" height="315" src="https://www.youtube.com/embed/TgPMkrUY8w4" frameborder="0" allowfullscreen></iframe>
+<body style='text-align:center; padding-top:50px;' id='body'>
+    <div class="center" id="center">
+        <p id="question"></p>
+        <input type="hidden" id="question_number" value="0" />
+        <div class="button" onClick="Answer(1);">Да</div>
+        <div class="button" onClick="Answer(0);">Нет</div>
+        <p id="paginator">1/80</p>
+    </div>
 </body>
 </html>
